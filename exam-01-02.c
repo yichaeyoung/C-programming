7463e15664751660d150e85581edc71a11229dd0
yichaeyoung <126742382+yichaeyoung@users.noreply.github.com>
2023-03-29 17:23:56 +0900

    Add files via upload

---
+++ exam-01-02.c
@@ -0,0 +1,15 @@
+/*
+파일명: exam-01-02.c
+작성자: 강윤호
+수정날짜: 2020-10-20
+기능: printf 함수를 통한 문자열 출력
+*/
+
+#include <stdio.h> //표준 함수 사용을 위한 헤더 파일 선언
+
+int main() {
+	printf("Hello!"); //Hello!를 출력합니다.
+
+		return 0;
+
+}
