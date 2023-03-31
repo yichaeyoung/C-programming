248db0ba322a0e5a4d5e9c30b41adc0743fd9eaf
yichaeyoung <126742382+yichaeyoung@users.noreply.github.com>
2023-03-31 10:13:46 +0900

    Add files via upload

---
+++ exam-02-02.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main() {
+	int num = 3;
+	int number5 = 5;
+	int num_ber = 10;
+	int Number = 15;
+	/*사용 불가한 변수명(컴파일 에러 유발)
+	int hello boy = 3;
+	int hi#$ = 5;
+	int 8number = 8;
+	int short = 3;*/
+
+	return 0;
+}
