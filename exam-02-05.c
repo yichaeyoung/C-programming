4cea7fcc835efd53b4b8414a562f65bb3293cbe8
yichaeyoung <126742382+yichaeyoung@users.noreply.github.com>
2023-04-04 10:31:46 +0900

    Add files via upload

---
+++ exam-02-05.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+	char ch1 = 66, ch2 = 'B';
+	short sh1 = 67;
+	int in1 = 68;
+
+	printf("%c\n", ch1);
+	printf("%c\n", ch2);
+	printf("%c\n", sh1);
+	printf("%c\n", in1);
+
+	return 0;
+}
