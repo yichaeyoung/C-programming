058c27acc8c2422d683712a7b9bd26d0e915e514
yichaeyoung <126742382+yichaeyoung@users.noreply.github.com>
2023-04-06 16:41:51 +0900

    Add files via upload

---
+++ exam-02-07.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+	double number1 = 10;
+	int number2 = 1.2345;
+	short number3 = 70000;
+
+	printf("%f\n", number1);
+	printf("%d\n", number2);
+	printf("%d\n", number3);
+	
+	return 0;
+}
