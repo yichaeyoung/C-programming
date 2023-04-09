e7cac8f6242c55a6ea3f0caacb8cbbe82d7ba1ca
yichaeyoung <126742382+yichaeyoung@users.noreply.github.com>
2023-04-09 20:51:45 +0900

    Add files via upload

---
+++ exam-04-02.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+	printf("\"I am a boy.\"\n");
+	printf("I am a \'boy.\'\n");
+	printf("I\t am\t a\t boy.\n");
+	printf("I\bam a boy.\n");
+	printf("I am a boy\\\?\n");
+}
