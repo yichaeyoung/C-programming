8a88927838a85a141583c05824cdf2c2c8d5197a
yichaeyoung <126742382+yichaeyoung@users.noreply.github.com>
2023-04-09 20:52:04 +0900

    Add files via upload

---
+++ exam-04-03.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+	char character = 'X';
+	int inumber = 92;
+	double dnumber = 20.201005;
+
+	printf("%c\n", character);
+	printf("%d\n", character);
+	printf("%d\n", inumber);
+	printf("%x\n", inumber);
+	printf("%o\n", inumber);
+	printf("%f\n", dnumber);
+	printf("%e\n", dnumber);
+}
