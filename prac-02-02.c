2a4173112d1344e3ea38c4da65497596094aa702
yichaeyoung <126742382+yichaeyoung@users.noreply.github.com>
2023-04-09 20:52:55 +0900

    Add files via upload

---
+++ prac-02-02.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main() {
+	printf("%d\n", sizeof(int));
+	printf("%d\n", sizeof(char));
+	printf("%d\n", sizeof(short));
+	printf("%d\n", sizeof(50));
+	printf("%d\n", sizeof(float));
+	printf("%d\n", sizeof(3.14));
+
+	return 0;
+}
