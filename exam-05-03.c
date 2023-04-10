a4347ed6decbe2c0fb57c25451a9a7a6da905f14
yichaeyoung <126742382+yichaeyoung@users.noreply.github.com>
2023-04-10 15:30:11 +0900

    Add files via upload

---
+++ exam-05-03.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+	//변수의 선언
+	int a = 10, b = 11;
+
+	printf("a == b:%d\n", a == b);
+	printf("a > b:%d\n", a > b);
+	printf("a < b:%d\n", a < b);
+	printf("a >= b:%d\n", a >= b);
+	printf("a <= b:%d\n", a <= b);
+	printf("a != b:%d\n", a != b);
+
+	return 0;
+}
