b4a6a8eb2da2e8b5805ea1be2ace48beb5c01605
yichaeyoung <126742382+yichaeyoung@users.noreply.github.com>
2023-04-11 01:08:13 +0900

    Add files via upload

---
+++ prac-05-02.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+	int num1 = 27;
+
+	printf("%d\n", num1 >= 10);
+	printf("%d\n", num1 != 5);
+
+	printf("%d\n", num1 >= 27);
+	printf("%d\n", num1 == 27);
+	
+	printf("%d\n", num1 <= 30);
+	printf("%d\n", num1 <= 27);
+
+}
