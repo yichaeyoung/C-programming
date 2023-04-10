fbefffafb9ea51772a4d2b9d84a84e6fb8bc96ee
yichaeyoung <126742382+yichaeyoung@users.noreply.github.com>
2023-04-10 15:29:46 +0900

    Add files via upload

---
+++ exam-05-01.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+	int num1 = 7, num2 = 3;
+	printf("%d + %d = %d\n", num1, num2, num1 + num2);
+	printf("%d - %d = %d\n", num1, num2, num1 - num2);
+	printf("%d * %d = %d\n", num1, num2, num1 * num2);
+	printf("%d / %d = %d\n", num1, num2, num1 / num2);
+	printf("%d %% %d = %d\n", num1, num2, num1 % num2);
+
+	return 0;
+}
