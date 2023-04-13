82c5541cb20344e55019056817a27af130ea5d71
yichaeyoung <126742382+yichaeyoung@users.noreply.github.com>
2023-04-13 11:11:51 +0900

    Add files via upload

---
+++ exam-06-03.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+	int a = 3;
+	int b = 5;
+
+	printf("3 & 5 = %d\n", a & b);
+	printf("3 | 5 = %d\n", a | b);
+	printf("3 ^ 5 = %d\n", a ^ b);
+	printf("~3 = %d\n", ~a);
+
+	return 0;
+
+}
