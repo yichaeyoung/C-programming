e1b289a9c066d881f39aeb9a51e29b86a8a0d005
yichaeyoung <126742382+yichaeyoung@users.noreply.github.com>
2023-04-13 11:13:48 +0900

    Add files via upload

---
+++ exam-07-01.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+	int num = 3;
+	if (num < 3)
+		printf("num is smaller than 3!");
+	
+	if (num == 3)
+		printf("num is 3!");
+
+	if (num > 3)
+		printf("num is bigger than 3!");
+
+	return 0;
+}
