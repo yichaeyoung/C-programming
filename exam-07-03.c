866a35a65fe48e3ca72f24d39589b0fe45c07821
yichaeyoung <126742382+yichaeyoung@users.noreply.github.com>
2023-04-14 14:54:04 +0900

    Add files via upload

---
+++ exam-07-03.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+	char alpha = 'C';
+
+	if (alpha == 'A')
+	{
+		printf("alpha is A.\n");
+		printf("I like apple.");
+	}
+	else if (alpha == 'B')
+	{
+		printf("alpha is B\n");
+		printf("I like banana.");
+	}
+	else if (alpha == 'C')
+	{
+		printf("alpha is C.\n");
+		printf("I like carrot.");
+	}
+	else
+	{
+		printf("I hate fruit & vegetable");
+
+	 }
+
+	return 0;
+}
