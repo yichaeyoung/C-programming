45ecf65da95072803dcb9404a52a0173ba5c6ec9
yichaeyoung <126742382+yichaeyoung@users.noreply.github.com>
2023-04-16 12:07:22 +0900

    Add files via upload

---
+++ exam-09-01.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+	//길이 정보가 포함된 배열의 선언 및 초기화
+	int numbers[3] = { 1,2,3 };
+
+	//길이 정보가 포함되지 않은 배열의 선언 및 초기화
+	char character[] = { 'a','b','c','d','e' };
+
+	return 0;
+}
