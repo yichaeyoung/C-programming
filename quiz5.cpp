2c791db88a8a4cb08d896f5f67b5effaa69163c2
yichaeyoung <126742382+yichaeyoung@users.noreply.github.com>
2023-04-16 12:12:42 +0900

    Add files via upload

---
+++ quiz5.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+/*/int main() {
+	int kor, eng, mat, sum = 0;	//더 이상 변수 없음
+	
+
+	cout << "국영수 점수 입력: ";
+	cin >> kor >> eng >> mat;
+
+	sum = kor + eng + mat;
+	cout << "합계 = " << sum << endl;
+
+	cout << "평균 = " << sum / 3.0 << endl;
+
+	
+}*/
+
+int main() {
+
+	int kor, eng, mat, sum = 0;
+
+	cout << "국영수 점수 입력: ";
+	cin >> kor >> eng >> mat;
+	
+	sum;
+
+	sum = kor + eng + mat;
+
+	cout << "합계 = " << sum << endl;
+	cout << "평균 = " << sum / 3.0;
+
+	return 0;
+}
