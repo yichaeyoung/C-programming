8898660932ff8133cbed712f6ec33586d8de104d
yichaeyoung <126742382+yichaeyoung@users.noreply.github.com>
2023-04-17 16:20:33 +0900

    Add files via upload

--- prac3.cpp
+++ prac3.cpp
@@ -1,17 +1,32 @@
 #include <iostream>
 using namespace std;
 
+class Circle {
+public:
+	int radius;
+	Circle();
+	Circle(int r);
+	double getArea();
+};
+Circle::Circle() {
+	radius = 1;
+	cout << "반지름 " << radius << "원 생성" << endl;
+}
+
+Circle::Circle(int r) {
+	radius = r;
+	cout << "반지름 " << radius << "원 생성" << endl;
+}
+double Circle::getArea() {
+	return
+	 3.14 * radius * radius;
+}
 int main() {
-	cout << "너비를 입력하세요 :";
+	Circle donut;
+	double area = donut.getArea();
+	cout << "donut 면적은 " << area << endl;
 
-	int width;
-	cin >> width;
-
-	cout << "높이를 입력하세요: ";
-
-	int height;
-	cin >> height;
-
-	int area = width * height;
-	cout << "면적은 " << area << "\n";
+	Circle pizza(30);
+	area = pizza.getArea();
+	cout << "pizza 면적은 " << area << endl;
 }
