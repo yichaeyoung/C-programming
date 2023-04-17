a1fc4fc435c97cbaebf016a654517e90802dcf86
yichaeyoung <126742382+yichaeyoung@users.noreply.github.com>
2023-04-17 16:20:06 +0900

    Add files via upload

--- prac1.cpp
+++ prac1.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
+using namespace std;
+class Circle {
+public:	//클래스 내, 외부 모두 접근 허용
+		int radius;		//멤버 변수
+	Circle();			//생성자
+	~Circle();			//소멸자
+	double getArea();	//멤버 함수
+};
+Circle::Circle() { radius = 1; }	//circle클래스 안의 circle 생성자
+Circle::~Circle() { cout << "bye" << radius << endl; }	//circle클래스 안의 circle 소멸자
+double Circle::getArea() {
+	return 3.141592 * radius * radius;
+}
 
 int main() {
-	std::cout << "Hello\n";
-	std::cout << "첫 번째 맛보기입니다" << std::endl;
+	Circle donut;
+	double area = donut.getArea();
+	cout << "donut 면적은 " << area << endl;
 	
-	return 0;
+	Circle pizza;
+	pizza.radius = 10;
+	cout << "pizza 면적은 " << pizza.getArea() << endl;
 }
