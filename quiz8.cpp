4c62baea46b99256b9774eb399c6869525f0d29b
yichaeyoung <126742382+yichaeyoung@users.noreply.github.com>
2023-05-16 12:54:07 +0900

    Add files via upload

--- quiz8.cpp
+++ quiz8.cpp
@@ -1,41 +1,16 @@
 #include <iostream>
-#include <string>
+#include <fstream>
 using namespace std;
 int main() {
 
-	string question[] = { "개","고양이","기린","코끼리","표범" };
-	string answer[] = { "dog","cat","giraffe","elephant","leopard" };
-	string ox;	//맞을 때 마다 O, 틀릴 때마다 X 추가
-	int score(0);
-	
-	cout << "영단어로 바꾸세요." << endl;
-	for (int i = 0; i < 5; i++) {
-		/*cout << question[i] << ": ";
-		string a;
-		getline(cin, a);
+	double arr[31];
+	ifstream fin("sort_before.txt");
+	ofstream fout("sort_after.txt");
 
-		if (a == answer[i]) {
-			ox += "O";
-			score += 20;
-		}
+	for (int i = 0; i < 31; i++)fin >> arr[i];
+	for (int i = 0; i < 31; i++)fout << arr[i];
 
-		else {
-			ox += "X";
-			score += 0;
-		}*/
-		cout << question[i] << ": ";
-		string a;
-		getline(cin, a);
-		
-		if (answer[i] == a) {
-			ox += 'O';	//변수 ox가 문자열 배열이 아닌 문자열으로만 표현됨
-			score += 20;
-		}
-		else {
-			ox += 'X';
-		}
-	}
+	fin.close();
+	return 0;
 
-	cout << "==>" << ox << " " << score << "점입니다." << endl;
 }
-
